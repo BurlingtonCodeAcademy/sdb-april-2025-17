53910c896bf6d2112e7776a78a6514f280692b67
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-28 17:33:11 -0500

    first method example

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("happy wednesday")
+// object method example
+const dog = {
+    name: "Buddy",
+    breed: "Golden Retriever",
+    bark: function () {
+        console.log("The dog goes WOOF!")
+    }
+}
+
+console.log("Dog name", dog.name); // Buddy
+console.log("Dog breed", dog.breed); // Golden Retriever
+dog.bark(); // The dog goes WOOF!
