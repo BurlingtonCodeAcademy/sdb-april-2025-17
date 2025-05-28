4fef67fa158f7963bab0fd9108189214b13d01eb
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-28 17:52:46 -0500

    new object example

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // object method example
 const dog = {
-    name: "Buddy",
+    name: "Rover",
     breed: "Golden Retriever",
     bark: function () {
         console.log("The dog goes WOOF!")
+    },
+    speak: function () {
+        console.log(`${this.name} can speak!`);
     }
 }
 
 console.log("Dog name", dog.name); // Buddy
 console.log("Dog breed", dog.breed); // Golden Retriever
 dog.bark(); // The dog goes WOOF!
+dog.speak(); // Buddy can speak!
+
+const cat = {
+    name: "Terrence",
+    color: "Brown",
+    introduce: function () {
+        console.log(`I am a ${this.color} cat named ${this.name}.`)
+    },
+
+    // dog says hello to cat
+    theySpeak: function () {
+        console.log(`${dog.name} says hello to ${this.name}!`)
+    }
+}
+
+console.log("Cat name", cat.name); // Terrence
+console.log("Cat color", cat.color); // Brown
+cat.introduce()
+cat.theySpeak();
