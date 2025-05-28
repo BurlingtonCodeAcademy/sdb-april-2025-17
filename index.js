7cef677e1b47b90c16d276d45730fce7d63a59db
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-28 18:22:51 -0500

    some penguin

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ console.log("Dog breed", dog.breed); // Golden Retriever
 dog.bark(); // The dog goes WOOF!
 dog.speak(); // Buddy can speak!
 
+const horse = {
+    name: "Spirit",
+}
+
+const penguin = {
+    speed: "fast",
+    color: "black and white",
+    swim: function () {
+        console.log(`The penguin swims at a ${this.speed} speed!`);
+    },
+}
+
 const cat = {
     name: "Terrence",
     color: "Brown",
@@ -24,7 +36,16 @@ const cat = {
 
     // dog says hello to cat
     theySpeak: function () {
-        console.log(`${dog.name} says hello to ${this.name}!`)
+        console.log(`${dog.name} says hello to ${cat.name}!`)
+    },
+    likesToEat: function (food) {
+        console.log(`${this.name} likes to eat ${food}`)
+    },
+    likesToFight: function (x) {
+        console.log(`${this.name} likes to fight ${x.speed} penguins!`)
+    },
+    catMath: function (num1, num2) {
+        return num1 + num2 + "meow"
     }
 }
 
@@ -32,3 +53,8 @@ console.log("Cat name", cat.name); // Terrence
 console.log("Cat color", cat.color); // Brown
 cat.introduce()
 cat.theySpeak();
+
+cat.likesToEat("fish and chips");
+
+cat.likesToFight(penguin)
+console.log(cat.catMath(1, 2)) // 3meow
